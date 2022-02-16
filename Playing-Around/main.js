bf4bf1ccf2dec33598ab5d8ecc3880c8df04298b
Joshua Smith <jcsmith2393@gmail.com>
2022-02-16 14:54:27 -0600

    Add files via upload

---
+++ Playing-Around/main.js
@@ -0,0 +1,7 @@
+const heading = document.querySelector('h1'); 
+const bodyEl = document.querySelector('body');
+function changeColor(){
+    bodyEl.style.backgroundColor='black';
+    heading.style.color='red';
+    
+}
