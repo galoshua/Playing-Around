1138beef446d2445fbf2803f6831098cbe54e882
Joshua Smith <jcsmith2393@gmail.com>
2022-02-17 20:40:17 -0600

    Add files via upload

---
+++ C02__JavaScript-Foundations-Part-1__JavaScript/Class/exercise.js
@@ -0,0 +1,7 @@
+let string = 'String';
+let number =1;
+let hitMe = true;
+let mrsJackson = null;
+let mrJackson ='';
+let ruby ={name:Chris, age:23};
+let dog = [7,'the thing',{name:John, pickles:false}] 
